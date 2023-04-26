9d90fa448c14df24cb8031bde1a9f0589f15f804
João Elias Maccari <joaomaccari@hotmail.com>
2023-04-26 13:12:51 -0300

    att

---
+++ Flap Bird Clone/Assets/scripts/testeScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Olá Mundo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
